8bed61038beaa974aeeabcd52e2f420de610fc47
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-01-29 11:54:45 +0300

    feat: create switch-language.js

---
+++ portfolio/js/modules/switch-language.js
@@ -0,0 +1,61 @@
+import i18Obj from './translate.js';
+
+// Local-storage---------------------------
+
+const ru = document.querySelector('#ru');
+const en = document.querySelector('#en');
+
+const getLocalStorage = function () {
+    if (localStorage.getItem('lang')) {
+        const lang = localStorage.getItem('lang');
+        getTranslate(lang);
+        if (lang === 'ru') {
+            ru.setAttribute('checked', 'checked');
+        }
+        if (lang === 'en') {
+            en.setAttribute('checked', 'checked');
+        }
+    } else {
+        en.setAttribute('checked', 'checked');
+    }
+
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme == "light") {
+        document.body.classList.add("light-theme");
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+window.addEventListener('load', getLocalStorage);
+
+// Switch language---------------------------
+
+const getString = function (language, key) {
+    if (i18Obj[language].hasOwnProperty(key)) {
+        return i18Obj[language][key];
+    } else {
+        return key;
+    }
+}
+
+const getTranslate = function (language) {
+    const dataElements = document.querySelectorAll('[data-i18]');
+    const placeholdersElements = document.querySelectorAll('[data-placeholder]');
+
+    for (const currentElement of dataElements) {
+        currentElement.innerHTML = getString(language, currentElement.getAttribute('data-i18'));
+    }
+
+    for (const placeholder of placeholdersElements) {
+        placeholder.setAttribute('placeholder', getString(language, placeholder.getAttribute('data-placeholder')));
+    }
+}
+
+const saveLang = function (event) {
+    let lang = event.target.id;
+    localStorage.setItem('lang', lang);
+    getTranslate(lang);
+}
+
+ru.addEventListener('click', saveLang);
+en.addEventListener('click', saveLang);
