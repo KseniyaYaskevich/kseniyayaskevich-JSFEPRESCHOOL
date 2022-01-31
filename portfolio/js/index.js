ef22b7c809d5f23dd3dc7204128266c73855aee9
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-01-31 13:17:09 +0300

    fix: code review

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -2,6 +2,34 @@ import './modules/menu.js';
 import './modules/switch-language.js';
 import './modules/theme-toggle.js';
 import './modules/portfolio-images.js';
+import * as storage from './modules/storage.js';
+import { getTranslate } from './modules/switch-language.js';
+
+const getLocalStorage = () => {
+    const lang = storage.getItem('lang');
+
+    if (lang) {
+        getTranslate(lang);
+
+        if (lang === 'ru') {
+            ru.checked = true;
+        }
+        if (lang === 'en') {
+            en.checked = true;
+        }
+    } else {
+        en.checked = true;
+    }
+
+    const currentTheme = storage.getItem("theme");
+
+    if (currentTheme == "light") {
+        document.body.classList.add("light-theme");
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+window.addEventListener('load', getLocalStorage);
 
 // Self-check---------------------------
 
--- portfolio/js/modules/switch-language.js
+++ portfolio/js/modules/switch-language.js
@@ -1,45 +1,17 @@
 import i18Obj from './translate.js';
-
-// Local-storage---------------------------
+import * as storage from './storage.js'
 
 const ru = document.querySelector('#ru');
 const en = document.querySelector('#en');
 
-const getLocalStorage = function () {
-    if (localStorage.getItem('lang')) {
-        const lang = localStorage.getItem('lang');
-        getTranslate(lang);
-        if (lang === 'ru') {
-            ru.checked = true;
-        }
-        if (lang === 'en') {
-            en.checked = true;
-        }
-    } else {
-        en.checked = true;
-    }
-
-    const currentTheme = localStorage.getItem("theme");
-
-    if (currentTheme == "light") {
-        document.body.classList.add("light-theme");
-        document.body.classList.remove("dark-theme");
-    }
-}
-
-window.addEventListener('load', getLocalStorage);
-
-// Switch language---------------------------
-
-const getString = function (language, key) {
+const getString = (language, key) => {
     if (i18Obj[language].hasOwnProperty(key)) {
         return i18Obj[language][key];
-    } else {
-        return key;
     }
+    return key;
 }
 
-const getTranslate = function (language) {
+export const getTranslate = (language) => {
     const dataElements = document.querySelectorAll('[data-i18]');
     const placeholdersElements = document.querySelectorAll('[data-placeholder]');
 
@@ -52,11 +24,11 @@ const getTranslate = function (language) {
     }
 }
 
-const saveLang = function (event) {
-    let lang = event.target.id;
-    localStorage.setItem('lang', lang);
+const saveLangHandler = (evt) => {
+    let lang = evt.target.id;
+    storage.setItem('lang', lang);
     getTranslate(lang);
 }
 
-ru.addEventListener('click', saveLang);
-en.addEventListener('click', saveLang);
+ru.addEventListener('click', saveLangHandler);
+en.addEventListener('click', saveLangHandler);
