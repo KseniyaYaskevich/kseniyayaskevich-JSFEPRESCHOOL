34d9807bb13d6e263207bbf3dc7bbcc6c4de6b58
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-02-13 12:24:06 +0300

    feat: add self-check

--- image-galery/js/index.js
+++ image-galery/js/index.js
@@ -3,45 +3,28 @@ import './modules/clear-search.js';
 
 getData();
 
-const showData = (data) => {
-    data.results.map((elem) => {
-        const imageElement = document.createElement('img');
-        imageElement.classList.add('gallery__image');
-        imageElement.src = elem.urls.regular;
-        imageElement.alt = elem.alt_description;
-        galleryContainer.append(imageElement);
-    })
-}
+console.log(`
+1. Вёрстка +10
+    + на странице есть несколько фото и строка поиска +5
+    + в футере приложения есть ссылка на гитхаб автора приложения, 
+    год создания приложения, логотип курса со ссылкой на курс +5
 
-const removeData = () => {
-    const imageElements = document.querySelectorAll('.gallery__image');
-    if (imageElements) {
-        imageElements.forEach(element => {
-            element.remove();
-        });
-    }
-}
+2. При загрузке приложения на странице отображаются полученные от API изображения +10
 
-const clearSearch = () => {
-    if (searchInput.value) {
-        searchInput.value = '';
-    }
-}
+3. Если в поле поиска ввести слово и отправить поисковый запрос, 
+на странице отобразятся изображения соответствующей тематики, если такие данные предоставляет API +10
 
-const getUrl = () => {
-    const searchQuery = searchInput.value;
-    const newSearchQuery = searchQuery.replace(/\s/g, ',');
-    url = `https://api.unsplash.com/search/photos?query=${newSearchQuery}&per_page=20&orientation=landscape&client_id=5nl4VW3783SH8KiriINIH-NPFaPNWD__wNmWN1GXv60`;
-    getData();
-}
+4. Поиск +30
+    + при открытии приложения курсор находится в поле ввода +5
+    + есть placeholder +5
+    + автозаполнение поля ввода отключено (нет выпадающего списка с предыдущими запросами) +5
+    + поисковый запрос можно отправить нажатием клавиши Enter +5
+    + после отправки поискового запроса и отображения результатов поиска, 
+    поисковый запрос продолжает отображаться в поле ввода +5
+    + в поле ввода есть крестик при клике по которому поисковый запрос 
+    из поля ввода удаляется и отображается placeholder +5
 
-searchButton.addEventListener('click', getUrl)
-
-searchInput.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.which == 13 || event.keyCode == 13) {
-        searchButton.click();
-    }
-});
-
-searchClear.addEventListener('click', clearSearch);
+5. Очень высокое качество оформления приложения и/или дополнительный не предусмотренный 
+в задании функционал, улучшающий качество приложения +10
+    + высокое качество оформления приложения предполагает собственное оригинальное 
+    оформление равное или отличающееся в лучшую сторону по сравнению с демо`)
